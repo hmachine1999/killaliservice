8b0df25e5b5fa65a9b1fa7e2ac1d253f6e8809e7
root <root@amazon-aws.lan>
2017-11-17 17:00:48 +0800

    sth

--- killaliservice.sh
+++ killaliservice.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 locate aliyun-|xargs rm -rf && rm -rf /usr/local/aegis/ 
-for A in $(ps aux | grep ali | grep -v grep | awk '{print $2}') 
+for A in $(ps aux | grep -i ali | grep -v grep | awk '{print $2}') 
 do 
       kill -9 $A; 
   done
